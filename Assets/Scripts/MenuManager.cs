a723c9bdd1154e91aebd3d349b1a57754db6b360
Adrian Nowowiejski <anowowiejski123@gmail.com>
2023-09-22 13:07:58 +0200

    menu update

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Menu_Play()
+    {
+
+    }
+
+    public void Menu_Mode()
+    {
+
+    }
+
+    public void Menu_Options()
+    {
+
+    }
+
+    public void Menu_Quit()
+    {
+
+    }
+}
