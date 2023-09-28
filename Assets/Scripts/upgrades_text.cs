c5c01e881d43ccfba866c89542b708dfdb7fca08
Tobkubos <tobkub7@gmail.com>
2023-09-28 11:52:31 +0200

    upgradez

--- Assets/Scripts/upgrades_text.cs
+++ Assets/Scripts/upgrades_text.cs
@@ -8,8 +8,57 @@ using UnityEngine.UI;
 public class upgrades_text : MonoBehaviour
 {
     public TextMeshProUGUI stats;
+    PlayerInfo player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInfo>();    
+    }
+
     void Update()
     {
-        stats.text = "dmg: NULL \n speed: NULL \n attack speed: NULL \n mana: NULL \n hp: NULL \n bullet size: NULL \n movement speed: NULL \n protection: NULL \n \n Enemies killed: NULL";
+        stats.text = "hp:" + player.hp.ToString() + 
+                 "\r\nmana:"+ player.mana.ToString() + 
+                 "\r\ndamage:" + player.GetDamage().ToString() + 
+                 "\r\ncrit Chance:" + player.GetCritChance().ToString() +
+                 "\r\ncrit Multiplier:" + player.GetCritMulti().ToString() +
+                 "\r\ndamage Multiplier:" + player.GetMultiplier().ToString() +
+                 "\r\nspeed:" + player.GetSpeed().ToString() + 
+                 "\r\n\r\nEnemies killed:" + "tu powinna byc funkcja lol";
+    }
+
+    void upgradeHP()
+    {
+
+    }
+
+    void upgradeMANA()
+    {
+
+    }
+
+    void upgradeDAMAGE()
+    {
+
+    }
+
+    void upgradeCRIT_CHANCE()
+    {
+
+    }
+
+    void upgradeCRIT_MULTIPLIER()
+    {
+
+    }
+
+    void upgradeDAMAGE_MULTIPLIER()
+    {
+
+    }
+
+    void upgradeSPEED()
+    {
+
     }
 }
